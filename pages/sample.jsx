ad873e5932fe045a66f0e852bd7867b100ffc25b
Admin <support@unlimitpotential.com>
2023-05-15 19:47:04 -0400

    h

--- pages/sample.jsx
+++ pages/sample.jsx
@@ -16,6 +16,9 @@ export default function HomePage() {
 		<>
 			<Color colors={colors} />
 			<Hero />
+			<span className="block h-auto grow-0 max-h-[164px] text-[rgba(22,11,54,1)] text-[64px] mt-[69px] border-solid border-0 max-sm:text-[rgba(22,11,54,1)] max-sm:ml-[-50px] max-sm:text-[30px] max-sm:mt-0">
+      <p>Design, test and manage your products better</p>
+    </span>
 			{/* <Looking /> */}
 			<FeaturedProjects />
 			<About />
