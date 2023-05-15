58921b1ef501b6edc323ca123917ff4f68e9706b
Admin <support@unlimitpotential.com>
2023-05-15 16:06:27 -0400

    s

--- components/utils/icon.util.jsx
+++ components/utils/icon.util.jsx
@@ -6,7 +6,6 @@ const { library, config } = require('@fortawesome/fontawesome-svg-core')
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fas } from '@fortawesome/pro-solid-svg-icons'
 import { fat } from '@fortawesome/pro-thin-svg-icons'
-import { fad } from '@fortawesome/pro-duotone-svg-icons'
 import { far } from '@fortawesome/pro-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
