0ff470cafea0aabf504c09cdb907a85da8ddc1be
Admin <support@unlimitpotential.com>
2023-05-16 22:24:56 -0400

    u

--- pages/sample.jsx
+++ pages/sample.jsx
@@ -4,10 +4,50 @@ import About 		from '../components/sections/index/about'
 import Technical 	from '../components/sections/index/technical'
 import Career 		from '../components/sections/index/career'
 import FeaturedProjects	from '../components/sections/projects/featured'
+import Hero from '../components/hero'
 
 import Color 		from '../components/utils/page.colors.util'
 
 import colors 		from '../content/index/_colors.json'
+
+import { Box, Flex, SimpleGrid, useBreakpointValue } from '@chakra-ui/react'
+import {
+  Container,
+  Heading,
+  Text,
+  HStack,
+  VStack,
+  Stack,
+  Link,
+  Button,
+  ButtonGroup,
+  useColorModeValue,
+  chakra,
+  Avatar,
+  VisuallyHidden,
+  Img,
+  Grid,
+  GridItem,
+  useDisclosure,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Tab,
+  useClipboard,
+  IconButton,
+  useTheme,
+  Tag,
+  Wrap,
+  WrapItem,
+  Icon,
+  Center,
+  Tooltip,
+  AspectRatio,
+  Badge,
+  DarkMode,
+  Card,
+} from '@chakra-ui/react'
 
 //
 export default function HomePage() {
@@ -15,7 +55,105 @@ export default function HomePage() {
 	return (
 		<><>
 			<Color colors={colors} />
-			<Hero />
+      <Stack
+              direction="column"
+              alignItems="center"
+              position="relative"
+              zIndex="2"
+            >
+              <Hero
+                as={Stack}
+                id="home"
+                alignItems="center"
+                textAlign="center"
+                title={
+                  <FallInPlace textAlign="center" initialInView>
+                    Build intuitive apps that your customers will love
+                  </FallInPlace>
+                }
+                description={
+                  <FallInPlace
+                    delay={0.4}
+                    fontWeight="medium"
+                    textAlign="center"
+                    fontSize={{ base: 'md', md: 'xl', lg: '2xl' }}
+                    initialInView
+                  >
+                    Saas UI is a <Em>React component library</Em> and{' '}
+                    <Em>starterkit</Em>
+                    <Br display={{ base: 'none', md: 'inline' }} /> that
+                    doesn&apos;t get in your way and helps you{' '}
+                    <Br display={{ base: 'none', md: 'inline' }} /> build
+                    intuitive SaaS products with speed.
+                  </FallInPlace>
+                }
+              >
+                <FallInPlace delay={0.8} initialInView>
+                  <HStack pt="12" pb="12" spacing="8" justifyContent="center">
+                    <HStack>
+                      <ReactLogo height="24px" />
+                      <Text fontWeight="medium">React</Text>
+                    </HStack>
+                    <ChakraLogo height="24px" />
+                  </HStack>
+                </FallInPlace>
+                <FallInPlace delay={1} initialInView>
+                  <ButtonGroup spacing={4} alignItems="center">
+                    <ButtonLink colorScheme="primary" size="lg" href="/pricing">
+                      Buy Pro
+                    </ButtonLink>
+                    <ButtonLink
+                      size="lg"
+                      href="/docs"
+                      variant="outline"
+                      _hover={{
+                        bg: 'whiteAlpha.200',
+                      }}
+                      rightIcon={
+                        <Icon
+                          as={FiArrowRight}
+                          sx={{
+                            transitionProperty: 'common',
+                            transitionDuration: 'normal',
+                            '.chakra-button:hover &': {
+                              transform: 'translate(5px)',
+                            },
+                          }}
+                        />
+                      }
+                    >
+                      Read documentation
+                    </ButtonLink>
+                  </ButtonGroup>
+                </FallInPlace>
+              </Hero>
+              <Box
+                width="80vw"
+                maxW="1100px"
+                margin="0 auto"
+                alignItems="center"
+                position="relative"
+                zIndex="2"
+              >
+                <FallInPlace
+                  delay={1.4}
+                  threshold={0.2}
+                  onChange={(inView) => setAnimate(inView)}
+                >
+                  <AspectRatio ratio={1100 / 698}>
+                    <Image
+                      src="/screenshots/list.png"
+                      // layout="fill"
+                      width={1100}
+                      height={698}
+                      alt="Screenshot of a ListPage in Saas UI Pro"
+                      quality="75"
+                      priority
+                    />
+                  </AspectRatio>
+                </FallInPlace>
+              </Box>
+            </Stack>
 
 			<span className="span">
 				<p>Design, test and manage your products better</p>
